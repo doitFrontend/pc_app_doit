63dd89c171c70ccd0016f51dd03f256424bbd9bf
yuanairui85 <yuanairui85@126.com>
2019-10-25 17:54:52 +0800

    原购物车改出个bug,先重新提交

--- src/components/ButtonGroups.vue
+++ src/components/ButtonGroups.vue
@@ -1,9 +1,15 @@
 <template>
   <ButtonGroup shape="circle">
-    <Button class="btn-remove" style="font-weight: 700" @click="minusNum(item)"><Icon type="md-remove" size="20" color="#bebebe" style="position: relative;top:-3px" /></Button>
-    <Button  type="primary">{{item.num}}</Button>
-    <Button  class="addnum" @click="addNum(item)"><Icon type="md-add" size="20" color="#fff"  style="position: relative;top:-3px"/></Button>
-    <!-- <Button  @click="addNum(item, 'true')" style="width:100px;font-size:14px;border-radius: 4px;color: #fff;background: #00A1E9;margin-right:5px;margin-top:3px;">加入购物车</Button> -->
+    <Button v-show="isShow" class="btn-remove" style="font-weight: 700" @click="minusNum(item)"><Icon type="md-remove" size="20" color="#bebebe" style="position: relative;top:-3px" /></Button>
+    <Button v-show="isShow" type="primary">{{item.num}}</Button>
+    <transition
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @after-enter="afterEnter">
+      <div v-show="isBallShow" class="ball"></div>
+    </transition>
+    <Button v-if="isShow" class="addnum" @click="addNum(item)"><Icon type="md-add" size="20" color="#fff"  style="position: relative;top:-3px"/></Button>
+    <Button v-else @click="addNum(item, 'true')" style="width:100px;font-size:14px;border-radius: 4px;color: #fff;background: #00A1E9;margin-right:5px;margin-top:3px;">加入购物车</Button>
   </ButtonGroup>
 </template>
 <script>
@@ -16,18 +22,37 @@ export default {
   },
   data() {
     return {
+      isBallShow: false,
+      isShow: false,
     };
   },
   computed: {},
   methods: {
-    addNum(item) {
+    addNum(item, b) {
+      b && (this.isShow = true);
+      this.isBallShow = true;
       this.$emit('countSum', { item, sign: 'ADD' });
     },
     minusNum(item) {
       if (item.num === 0) { // 逻辑里面进一步判断是否可以继续减
         return;
       }
+      (item.num === 1) && (this.isShow = false);
       this.$emit('countSum', { item, sign: 'MINUS' });
+    },
+    // 点击动画效果
+    beforeEnter(el) {
+      el.style.transform = 'translate(0, 0)';
+    },
+    enter(el, done) {
+      el.offsetWidth;
+      let shopCart = document.querySelector('#cart');
+      el.style.transform = `translate(${shopCart.offsetLeft}px, ${shopCart.offsetTop}px)`;
+      el.style.transition = 'all .4s ease-in-out';
+      done(); // 去除停顿时间
+    },
+    afterEnter(el) {
+      this.isBallShow = false;
     },
   },
 };
